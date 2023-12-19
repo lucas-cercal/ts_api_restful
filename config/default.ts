1b21de32e181eace93c11eed3485685cc0408428
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-19 16:51:16 -0300

    feat: connecting to the database

--- config/default.ts
+++ config/default.ts
@@ -1,3 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config()
+
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
+
 export default {
-  port: 3000
+  port: 3000,
+  dbUri: `mongodb+srv://${dbUser}:${dbPassword}@cluster0.ive6cex.mongodb.net/`
 }
