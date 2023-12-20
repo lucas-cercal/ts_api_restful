bafc5b1828f51ec7791f77aa5048747e46929d64
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-20 11:02:19 -0300

    feat: error validation middleware

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,12 @@
 import { Router, Request, Response } from 'express'
 import { createMovie } from './controllers/movieController'
 
+// Validations
+import { validate } from './middleware/handleValidator'
+
 const router = Router()
 
 export default router.get('/test', (req: Request, res: Response) => {
   res.status(200).send('API Working!')
 })
-  .post('/movie', createMovie)
+  .post('/movie', validate, createMovie)
